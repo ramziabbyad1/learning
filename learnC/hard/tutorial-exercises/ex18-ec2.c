b895d2cf7651be35e8003da8ce2f6d6ee7ec060a
ramzi <ramzi.abbyad@gmail.com>
2016-07-28 16:45:15 -0500

    ex18-ec2

--- learnC/hard/tutorial-exercises/ex18-ec2.c
+++ learnC/hard/tutorial-exercises/ex18-ec2.c
@@ -18,6 +18,38 @@ die(const char *message)
 }
 
 typedef int (*compare_cb)(int a, int b);
+typedef int * (*sort_fn) (int *numbers, int count, compare_cb cmp);
+typedef int * (*sort_lin) (int *numbers, int count, int limit);
+
+int *
+counting_sort(int *numbers, int count, int limit) 
+{
+	int *count_order = (int *) calloc(sizeof(int)*limit);
+	int *sorted = (int *) calloc(sizeof(int)*count);
+	int *a = numbers;
+	int i;
+	//do a raw count of numbers
+	for(i = 0; i < count; i++) {
+		count_order[a[i]]++;
+	}
+	//do count of numbers less than or equal to i
+	for(i = 0; i < limit; i++) {
+		count_order[i] += count_order[i-1];
+	}
+	//
+	int k;
+	for(k = count-1; k >= 0; k--) {
+		sorted[count_order[a[k]]] = a[k];
+		count_order[a[k]]--;
+	}
+	return sorted;	
+}
+
+int *
+insertion_sort(int *numbers, int count, compare_cb cmp)
+{
+	
+}
 
 int *
 bubble_sort(int *numbers, int count, compare_cb cmp)
@@ -67,9 +99,9 @@ strange_order(int a, int b)
 }
 
 void
-test_sorting(int *numbers, int count, compare_cb cmp)
+test_sorting(int *numbers, int count, sort_fn sort, compare_cb cmp)
 {
-	int i = 0;
+	/*int i = 0;
 	int *sorted = bubble_sort(numbers, count, cmp);
 
 	if(!sorted) die("Failed to sort as requested");
@@ -86,6 +118,7 @@ test_sorting(int *numbers, int count, compare_cb cmp)
 		printf("%02x:", data[i]);
 	}
 	printf("\n");
+	*/
 }
 
 int
@@ -111,9 +144,9 @@ main(int argc, char *argv[])
 		numbers[i] = atoi(inputs[i]);
 	}
 
-	test_sorting(numbers, count, sorted_order);
-	test_sorting(numbers, count, reverse_order);
-	test_sorting(numbers, count, strange_order);
+	//test_sorting(numbers, count, sorted_order);
+	//test_sorting(numbers, count, reverse_order);
+	//test_sorting(numbers, count, strange_order);
 
 	free(numbers);
 
